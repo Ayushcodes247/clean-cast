365df8f0c1c2876f71d5e3534c83e6a90aa3591b
Ayush Sharma <ayushsharma25072007@gmail.com>
2025-11-19 21:24:36 +0530

    Added fetch image for all type of user account.

--- backend/controllers/user/image.controller.js
+++ backend/controllers/user/image.controller.js
@@ -73,3 +73,69 @@ module.exports.uploadImage = async (req, res) => {
     });
   }
 };
+
+module.exports.fetchImage = async (req, res) => {
+  try {
+    const user = req.user;
+
+    if (!user || !user._id) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized user." });
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    const accountType = req.query.accountType;
+    const search = req.query.search;
+
+    const query = { uid: user._id };
+
+    if (accountType && ["public", "private"].includes(accountType)) {
+      query.accountType = accountType;
+    }
+
+    if (search) {
+      query.description = { $regex: search, $options: "i" };
+    }
+
+    const findImages = await imageModel
+      .find(query)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const totalImages = await imageModel.countDocuments(query);
+    const totalPages = Math.ceil(totalImages / limit);
+
+    if (findImages.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "No images found.",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "User images fetched successfully.",
+      pagination: {
+        currentPage: page,
+        totalPages,
+        limit,
+        totalImages,
+      },
+      filters: {
+        accountType: user?.accountType || "all",
+      },
+      images: findImages,
+    });
+  } catch (error) {
+    console.error("Error while fetching images:", error);
+    return res.status(500).json({
+      success: false,
+      message: error.message || "Internal server error.",
+    });
+  }
+};
